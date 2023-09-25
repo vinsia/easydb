5e0117b4ba7a8d73a4b83bd5e2fde081b6fdd44c
Yijian Li <yijian.li@Autras-MacBook-Air.local>
2023-09-25 19:01:24 +0800

    verify the skiplist bug

--- easydb/test.cc
+++ easydb/test.cc
@@ -12,13 +12,40 @@
 #include "easydb/skiplist.h"
 #include "easydb/utils/arena.h"
 
+using easydb::utils::Arena;
+
+struct Node {
+  int value_[1];
+};
+
+class TestNode {
+ public:
+  explicit TestNode(Arena* arena, int size)
+      : arena_(arena), node_(NewNode(size)){};
+
+  Node* NewNode(int size) {
+    std::cout << "2" << std::endl;
+    int kNodeAllocationSize = sizeof(Node) + sizeof(int) * (size - 1);
+    char* const node_memory = arena_->AllocateAligned(kNodeAllocationSize);
+    Node* node = new (node_memory) Node();
+    return node;
+  }
+
+ private:
+  Node* const node_;
+  Arena* const arena_;
+};
+
 int main() {
   easydb::Arena arena;
-  easydb::SkipList<int, int> skip_list = easydb::SkipList<int, int>(&arena);
-  std::cout << skip_list.GetMaxHeight() << std::endl;
-  skip_list.Insert(1, 2);
-
-  std::cout << skip_list.Contains(1) << std::endl;
-  std::cout << skip_list.Contains(2) << std::endl;
+  TestNode test_node(&arena, 10);
+  std::cout << "1" << std::endl;
   return 0;
+  // easydb::SkipList<int, int> skip_list = easydb::SkipList<int, int>(&arena);
+  // std::cout << skip_list.GetMaxHeight() << std::endl;
+  // skip_list.Insert(1, 2);
+
+  // std::cout << skip_list.Contains(1) << std::endl;
+  // std::cout << skip_list.Contains(2) << std::endl;
+  // return 0;
 }
